ee94e3b51a3ffacbacf45373c8f1c2b978551c20
Lorna <lroberts1980@yahoo.com>
2020-04-05 11:00:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const myDeck={
+deck:[],
+drawnCards:[],
+ suits: ['hearts', 'clubs', 'diamonds', 'spades'],
+values: '2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A',
+  initializeDeck() {  
+  const {
+    suits,
+     values,
+      deck
+    } = this;
+
+
+for (let value of values.split(',')) {
+  for (let suit of suits){
+    deck.push({
+      value,
+      suit
+    })
+  }
+}
+},
+//draws a single card
+drawCard() {
+ const card=this.deck.pop();
+ this.drawnCards.push(card);
+ return card;
+},
+
+/*
+drawMultiple(numCard) {
+  const cards = []; 
+  for (let i = 0; i< numCard; i++){
+    cards.push(this.drawCard());
+  }
+  return cards;
+},*/
+
+//fisher yates shuffle
+shuffle(){
+  const {deck} = this;
+  for (let i=deck.length-1; i >0; i--) {
+    let j = Math.floor(Math.random()*[i+1]);
+    [deck[i], deck[j]]= [deck[j], deck[i]];
+    console.log(deck);
+  }
+}
+
+}
+
